fc5998caa8cb1901bab1a5ee0283dee2b5af0150
Anasieze Ikenna <152716902+Donaldnaz@users.noreply.github.com>
2026-09-15 15:21:39 -0230

    Update API URL and restore app state management

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Update this with your actual API Gateway endpoint
-const API_URL = "https://zfx3nu2e09.execute-api.us-west-1.amazonaws.com/appications";
+const API_URL = "https://zj0hg7c37g.execute-api.us-east-1.amazonaws.com/appications";
 
 // State management
 let applications = [];
